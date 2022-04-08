41bdf1713ac09348a9604b34fdf6419c8866b43a
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-04-09 00:21:10 +0430

    Users and User pages completed

---
+++ pages/users.js
@@ -0,0 +1,42 @@
+import Layout from "../Layout/Layout";
+import { getUsers } from "../lib/api";
+import styles from "../styles/Users.module.css";
+import Link from "next/link";
+import Head from "next/head";
+
+const User = ({ user }) => {
+  return (
+    <Link href={`/users/${user.id}`}>
+      <div className={styles.userBox}>
+        <span>{user.name}</span>
+        <span>{user.email}</span>
+      </div>
+    </Link>
+  );
+};
+
+const Users = ({ users }) => {
+  return (
+    <Layout>
+      <main className="container">
+        <Head>
+          <title>Users</title>
+        </Head>
+        <div className={styles.users}>
+          {users.map((user) => (
+            <User key={user.id} user={user} />
+          ))}
+        </div>
+      </main>
+    </Layout>
+  );
+};
+
+export default Users;
+
+export async function getStaticProps() {
+  const users = await getUsers("users");
+  return {
+    props: { users },
+  };
+}
